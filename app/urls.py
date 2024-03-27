cfd9c913c827913cfbcf718c11d76e7991044dba
ulugbekpy <ulugbekpycpp@gmail.com>
2024-03-27 18:17:34 +0500

    basic template config

--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 
+from .views import test_view
+
 urlpatterns = [
+    path("test-view",test_view),
 ]
--- users/models.py
+++ users/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager
 
-# Create your models here.
+from core.models import BaseModel
+
+
+class AccountManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class Account(AbstractBaseUser,BaseModel):
+
+    class GenderChoices(models.TextChoices):
+        MALE = "M", "Male"
+        FEMALE = "F", "Female"
+
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    age = models.PositiveSmallIntegerField()
+    gender = models.CharField(
+        max_length=1,
+        choices=GenderChoices.choices,
+        default=GenderChoices.MALE,
+    )
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = AccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
